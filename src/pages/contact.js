9835609c1e730eda79c43eddbbc40f5e17de9fbc
Prasanth Vaaheeswaran <prasanthv@icloud.com>
2018-03-03 19:18:19 -0500

    Initial commit + initial pages

---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import Reach from "react";
+
+export default () => (
+  <div>
+    <h1>I'd love to talk! Email me at the address below</h1>
+    <p>
+      <a href="mailto:me@example.com">me@export.com</a>
+    </p>
+  </div>
+);
