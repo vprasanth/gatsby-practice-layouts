f7deee9f4ce319f2a04f1c8a2dae9f9bed381003
Prasanth Vaaheeswaran <prasanthv@icloud.com>
2018-03-03 19:47:51 -0500

    Add links to layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,26 @@
 import React from "react";
+import Link from "gatsby-link";
+
+const ListLink = (props) => (
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={props.to}>
+      {props.children}
+    </Link>
+  </li>
+);
 
 export default ({ children }) => (
-  <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
+  <div style={{ margin: `0 auto`, maxWidth: 650, padding: `1.25rem 1rem` }}>
+    <header style={{ marginBottom: `1.25rem` }}>
+      <Link to="/" style={{ textShawdow: `none`, backgroundImage: `none` }}>
+        <h3 style={{ display: `inline` }}>MySweetSite</h3>
+      </Link>
+      <ul style={{ listStyle: `none`, float: `right` }}>
+        <ListLink to="/">Home</ListLink>
+        <ListLink to="/about">About</ListLink>
+        <ListLink to="/contact">Contact</ListLink>
+      </ul>
+    </header>
     {children()}
   </div>
 );
